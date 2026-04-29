a23928006f80b37c6ce903cfdd196fa1b4d2a59e
Mauvis Ledford <switchstatement@gmail.com>
2026-04-29 22:30:23 +0800

    Fix volume 20 media sync

--- sogni-sync/sync.sh
+++ sogni-sync/sync.sh
@@ -1,1 +1,1 @@
-rsync -ar --progress --chmod=Du+rwx,go+rx,Fu+rw,go+r --exclude='.git' . sogni-api:/var/www/news.sogni.ai/sogni-sync/
+rsync -ar --progress --chmod=Du+rwx,Dgo+rx,Fu+rwx,Fgo+rx --exclude='.git' . sogni-api:/var/www/news.sogni.ai/sogni-sync/
